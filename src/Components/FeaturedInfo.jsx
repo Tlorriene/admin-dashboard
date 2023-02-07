ca47ce47d9833e6e45514e480633dbff6834527b
Tlorriene <tlorriene@icloud.com>
2023-02-07 23:57:11 +0000

    1st component build

---
+++ src/Components/FeaturedInfo.jsx
@@ -0,0 +1,12 @@
+import '../css/component/featuredInfo.css'
+import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
+
+function featuredInfo() {
+    return (
+        <div className="featured">
+            <div className="featuredItem">
+                <span className="featuredTitle">Revenue</span>
+            </div>
+        </div>
+    )
+}
