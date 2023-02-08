8f57a839053c337f124e5d10b7201c202fbd8248
Tlorriene <tlorriene@icloud.com>
2023-02-08 20:11:24 +0000

    building out sidebar menu

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -38,7 +38,34 @@ function Sidebar() {
                         </li>
                     </ul>
                 </div>
-                
+            <div className="sidebarMenu">
+                <h3 className="sidebarTitle">Quick Menu</h3>
+                <ul className="sidebarList">
+                    <Link to ="/users" className="link">
+                        <li className="sidebarListItem">
+                        <PermIdentity className="sidebarIcon"/>
+                        Users
+                        </li>
+                    </Link>
+                    <Link to="/products" className="link">
+                        <li className="sidebarListItem">
+                            <Storefront className="sidebaricon"/>
+                            Products
+                        </li>
+                        </Link>
+                        <li className="sidebarListItem">
+                            <AttachMoney className="sidebarIcon"/>
+                            Transactions
+                        </li>
+                        <li className="sidebarListItem">
+                            <BarChart className="sidebarIcon"/>
+                            Reports
+                        </li>
+                </ul>
+            </div>
+
+            
+               
             </div>
         </div>
     )
