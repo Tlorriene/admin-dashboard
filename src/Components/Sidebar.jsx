23dbc9b11811f84711ff813076788e615e4d492e
Tlorriene <tlorriene@icloud.com>
2023-02-08 21:18:24 +0000

    sidebar component completed

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -63,10 +63,43 @@ function Sidebar() {
                         </li>
                 </ul>
             </div>
-
-            
-               
+            <div className="sidebarListIem">
+                <h3 className="sidebarTitle">Notifications</h3>
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">
+                        <MailOutline className="sidebarIcon"/>
+                        Mail
+                    </li>
+                    <li className="sidebarListItem">
+                        <DynamicFeed className="sidebarIcon"/>
+                        Feedback
+                    </li>
+                    <li className="sidebarListItem">
+                        <ChatBubbleOutline className="sidebarIcon"/>
+                        Messages 
+                    </li>
+                </ul>
+            </div>
+            <div className="sidebarMenu">
+                <h3 className="sidebarTitle">Staff</h3>
+                <ul className ="sidebarList">
+                    <li className="sidebarListItem">
+                        <WorkOutline className="sidebarIcon"/>
+                        Manage
+                    </li>
+                    <li className="sidebarListItem">
+                        <Timeline className="sidebarIcon"/>
+                        Analytics
+                    </li>
+                    <li className="sidebarListItem">
+                        <Report className="sidebarIcon"/>
+                        Reports
+                    </li>
+                </ul>
             </div>
         </div>
-    )
+    </div>
+    );
 }
+
+export default Sidebar;
